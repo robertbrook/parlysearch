3e5359d78f3c08b2a6f10ac060a3dec7eba88cab
lizconlan <Liz@liz-conlans-macbook.local>
2009-06-08 16:00:59 +0100

    better way of removing extra spacing from search results

--- spec/views/parly_resources/search_spec.rb
+++ spec/views/parly_resources/search_spec.rb
@@ -6,14 +6,18 @@ describe "/parly_resources/index.html.haml" do
   before(:each) do
     parly_resource_98 = mock_model(ParlyResource)
     parly_resource_98.should_receive(:short_title).and_return("Short")
-    parly_resource_98.should_receive(:unique_description).twice.and_return("Unique")
+    parly_resource_98.should_receive(:unique_description).exactly(3).times.and_return("Unique")
     parly_resource_98.should_receive(:url).twice.and_return("MyString")
     parly_resource_99 = mock_model(ParlyResource)
     parly_resource_99.should_receive(:short_title).and_return("Short")
-    parly_resource_99.should_receive(:unique_description).twice.and_return("Unique")
+    parly_resource_99.should_receive(:unique_description).twice.and_return("")
     parly_resource_99.should_receive(:url).twice.and_return("MyString")
+    parly_resource_100 = mock_model(ParlyResource)
+    parly_resource_100.should_receive(:short_title).and_return("")
+    parly_resource_100.should_receive(:unique_description).and_return(nil)
+    parly_resource_100.should_receive(:url).twice.and_return("MyString")
 
-    assigns[:parly_resources] = [parly_resource_98, parly_resource_99]
+    assigns[:parly_resources] = [parly_resource_98, parly_resource_99, parly_resource_100]
 
     template.stub!(:object_url).and_return(parly_resource_path(parly_resource_99))
   end
