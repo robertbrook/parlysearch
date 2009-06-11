8f61c50c48a8ecde1c778d0620957e4575e7c39e
robmckinnon <rob@theyworkforyou.co.nz>
2009-06-11 18:16:53 +0100

    added sort by date - requires: rake solr:destroy_index ; ./script/console > ParlyResource.reindex

--- app/controllers/parly_resources_controller.rb
+++ app/controllers/parly_resources_controller.rb
@@ -7,6 +7,8 @@ class ParlyResourcesController < ResourceController::Base
 
   def search
     @search_query = params[:q]
+    @sort = params[:sort]
+
     if @search_query.blank?
       redirect_to root_url
     elsif params[:commit]
@@ -14,7 +16,7 @@ class ParlyResourcesController < ResourceController::Base
     else
       params[:page] ||= '1'
       @paginator = WillPaginate::Collection.create(params[:page], 10) do |pager|
-        @parly_resources, total = ParlyResource.search(@search_query, pager.offset, pager.per_page)
+        @parly_resources, total = ParlyResource.search(@search_query, pager.offset, pager.per_page, @sort)
         pager.replace(@parly_resources)
         pager.total_entries = total
       end
